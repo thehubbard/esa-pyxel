4d5f3a412be3a5d9455415fd6fed01fb8db88585
Frederic Lemmel <frederic.lemmel@esa.int>
2020-08-26 15:03:09 +0200

    + CHG: Add file '__main__.py'. Pyxel can be executed with 'python -m pyxel'

---
+++ pyxel/__main__.py
@@ -0,0 +1,25 @@
+#  Copyright (c) European Space Agency, 2017, 2018, 2019, 2020.
+#
+#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#  is part of this Pyxel package. No part of the package, including
+#  this file, may be copied, modified, propagated, or distributed except according to
+#  the terms contained in the file ‘LICENCE.txt’.
+
+"""Script to run Pyxel as a module.
+
+Examples
+--------
+$ python -m pyxel --version
+Pyxel, version ...
+
+$ python -m pyxel --help
+...
+
+$ python -m pyxel --config my_config.yaml
+...
+"""
+
+from pyxel.run import main
+
+if __name__ == "__main__":
+    main()
