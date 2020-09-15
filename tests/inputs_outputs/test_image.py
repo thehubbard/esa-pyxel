a912a3d272bf899d8232649201e2110a86342e94
Matej Arko <matej.arko@esa.int>
2020-09-15 11:45:27 +0200

    +CHG: added test_load_image_invalid

---
+++ tests/inputs_outputs/test_image.py
@@ -0,0 +1,14 @@
+#  Copyright (c) European Space Agency, 2017, 2018, 2019, 2020.
+#
+#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#  is part of this Pyxel package. No part of the package, including
+#  this file, may be copied, modified, propagated, or distributed except according to
+#  the terms contained in the file ‘LICENCE.txt’.
+
+from pyxel.inputs_outputs import load_image
+import pytest
+
+def test_load_image_invalid():
+    with pytest.raises(FileNotFoundError):
+         _ = load_image('dummy')
+
