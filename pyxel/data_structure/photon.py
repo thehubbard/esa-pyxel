3a82758dd316bd4e54bd81c16c705c0140720b88
Matej Arko <matej.arko@esa.int>
2020-09-18 14:44:34 +0200

    + CHG: Deleted array getters and setters in photon.py

--- pyxel/data_structure/photon.py
+++ pyxel/data_structure/photon.py
@@ -50,25 +50,6 @@ class Photon(Array):
 
         self._array = value
 
-    @property
-    def array(self) -> np.ndarray:
-        """TBW."""
-        if self._array is None:
-            raise RuntimeError(
-                "Photon array is not initialized ! "
-                "Please use a 'Photon Generation' model."
-            )
-
-        return self._array
-
-    @array.setter
-    def array(self, value: np.ndarray) -> None:
-        """TBW."""
-        self.validate_type(value)
-
-        # self.type = value.dtype
-        self._array = value
-
     # # TODO: This could be done in '__init__'
     # def new_array(self, new_array: np.ndarray) -> None:
     #     """TBW.
