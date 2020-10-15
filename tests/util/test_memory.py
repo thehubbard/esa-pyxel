e8f0b120866e3ee483595367141a486829db594d
Matej Arko <matej.arko@esa.int>
2020-10-15 11:57:18 +0200

    + CHG: update util __init__.py, started tests

---
+++ tests/util/test_memory.py
@@ -0,0 +1,16 @@
+#  Copyright (c) European Space Agency, 2017, 2018, 2019, 2020.
+#
+#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#  is part of this Pyxel package. No part of the package, including
+#  this file, may be copied, modified, propagated, or distributed except according to
+#  the terms contained in the file ‘LICENCE.txt’.
+#
+#
+import pytest
+from pyxel.util import get_size, memory_usage_details
+import numpy as np
+import pandas as pd
+import sys
+
+
+
