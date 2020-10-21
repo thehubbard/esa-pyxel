7e390fa6e8de5ba3e4158c213963bbfec10adce4
Matej Arko <matej.arko@esa.int>
2020-10-21 12:20:38 +0200

    + CHG: added documentation, rafactoring

--- pyxel/util/jupyxel.py
+++ pyxel/util/jupyxel.py
@@ -1,22 +1,42 @@
-#!/usr/bin/env python
-# coding: utf-8
+#  Copyright (c) European Space Agency, 2017, 2018, 2019, 2020.
+#
+#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#  is part of this Pyxel package. No part of the package, including
+#  this file, may be copied, modified, propagated, or distributed except according to
+#  the terms contained in the file ‘LICENCE.txt’.
+
+"""Tools for jupyter notebook visualization."""
+
+import typing as t
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 # # Display methods for detector objects in Jupyter notebook
 from IPython.display import Markdown, display
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-from pyxel.pipelines.model_group import ModelGroup
-import typing as t
+from pyxel.data_structure import Image, Photon, Pixel, Signal
+from pyxel.detectors import Detector
 from pyxel.pipelines import Processor
+from pyxel.pipelines.model_group import ModelGroup
 
 # ----------------------------------------------------------------------------------------------
 # Those two methods are used to display the contents of the configuration once loaded in pyxel
 
 
-def display_config(cfg, only="all"):
-    """DocString"""
+def display_config(cfg: dict, only: str = "all") -> None:
+    """Display configuration.
+
+    Parameters
+    ----------
+    cfg: dict
+    only: str
+
+    Returns
+    -------
+    None
+    """
     for k in cfg:
         if (only not in cfg.keys()) & (only != "all"):
             error = "Config file only contains following keys: " + str(cfg.keys())
@@ -34,7 +54,17 @@ def display_config(cfg, only="all"):
                 display_dict(cfg[k].__dict__)
 
 
-def display_dict(cfg):
+def display_dict(cfg: dict) -> None:
+    """Display configuration dictionary.
+
+    Parameters
+    ----------
+    cfg: dict
+
+    Returns
+    -------
+    None
+    """
     for k in cfg:
         display(Markdown("#### <font color=#0088FF>" + k + "</font>"))
         display(Markdown("\t" + str(cfg[k])))
@@ -44,9 +74,26 @@ def display_dict(cfg):
 # This method will display the parameters of a specific model
 
 
-def display_model(cfg, model_name: str) -> None:
+def display_model(
+    pipeline_container: t.Union[Processor, dict], model_name: str
+) -> None:
+    """Display model from configuration dictionary or Processor object.
+
+    Parameters
+    ----------
+    pipeline_container: Processor or dict
+    model_name: str
+
+    Returns
+    -------
+    None
+    """
     model_found = False
-    for value in cfg["pipeline"].__dict__.values():
+    if isinstance(pipeline_container, Processor):
+        pipeline = pipeline_container.pipeline
+    else:
+        pipeline = pipeline_container["pipeline"]
+    for value in pipeline.__dict__.values():
         if isinstance(value, ModelGroup):
             # value is a list of ModelFunction namespaces
             models_list = value.__dict__["models"]
@@ -73,12 +120,24 @@ def display_model(cfg, model_name: str) -> None:
         display(Markdown("<font color=red>" + model_name + " not found</font>"))
 
 
-# TODO: change cfg to processor
-# TODO: change type to isinstance
-# TODO: fix flake8
-def change_modelparam(cfg: dict, model_name: str, argument: str, changed_value: t.Any) -> None:
+def change_modelparam(
+    processor: Processor, model_name: str, argument: str, changed_value: t.Any
+) -> None:
+    """Change model parameter.
+
+    Parameters
+    ----------
+    processor: Processor
+    model_name: str
+    argument:str
+    changed_value
+
+    Returns
+    -------
+    None
+    """
     display(Markdown("## <font color=blue>" + model_name + "</font>"))
-    for value in cfg["pipeline"].__dict__.values():
+    for value in processor.__dict__.values():
         if isinstance(value, ModelGroup):
             for model in value.__dict__["models"]:
                 if model.name == model_name:
@@ -88,9 +147,21 @@ def change_modelparam(cfg: dict, model_name: str, argument: str, changed_value: 
                         print(model_name, "possess no argument named: ", value)
 
 
-def set_modelstate(cfg: dict, model_name: str, state: bool = True) -> None:
+def set_modelstate(processor: Processor, model_name: str, state: bool = True) -> None:
+    """Change model state (true/false).
+
+    Parameters
+    ----------
+    processor: Processor
+    model_name: str
+    state: bool
+
+    Returns
+    -------
+    None
+    """
     display(Markdown("## <font color=blue>" + model_name + "</font>"))
-    for value in cfg["pipeline"].__dict__.values():
+    for value in processor.pipeline.__dict__.values():
         if isinstance(value, ModelGroup):
             for model in value.__dict__["models"]:
                 if model.name == model_name:
@@ -116,10 +187,19 @@ def set_modelstate(cfg: dict, model_name: str, state: bool = True) -> None:
 # These method are used to display the detector object (all of the array Phton, pixel, signal and image)
 
 
-def display_array(data, axes, **kwargs):
-    """For a pair of axes, will display the image on the first one, the histogram on the second
-    :input: data, a 2D np.array
-            axes, a list of two axes in a figure
+def display_array(data: np.ndarray, axes: t.List[plt.axes], **kwargs: str) -> None:
+    """For a pair of axes, display the image on the first one, the histogram on the second.
+
+    Parameters
+    ----------
+    data: ndarray
+        A 2D np.array.
+    axes: list
+        A list of two axes in a figure.
+
+    Returns
+    -------
+    None
     """
     im = axes[0].imshow(data)
     axes[0].get_xaxis().set_visible(False)
@@ -136,19 +216,30 @@ def display_array(data, axes, **kwargs):
     axes[1].grid(True, alpha=0.5)
 
 
-def display_detector(detector, array="all"):
-    """DocString display_detector"""
-    if array != "all":
+def display_detector(
+    detector: Detector, array: t.Union[None, Photon, Pixel, Signal, Image] = None
+) -> None:
+    """Display detector.
+
+    Parameters
+    ----------
+    detector: Detector
+    array: str
+
+    Returns
+    -------
+    None
+    """
+    if array is not None:
         fig, axes = plt.subplots(1, 2, figsize=(15, 6))
         display_array(array.array, axes, label=str(array).split("<")[0])
     else:
         arrays = [detector.photon, detector.pixel, detector.signal, detector.image]
 
         fig, axes = plt.subplots(len(arrays), 2, figsize=(15, 6 * len(arrays)))
-        [
+
+        for idx, data in enumerate(arrays):
             display_array(data.array, axes[idx], label=str(data).split("<")[0])
-            for idx, data in enumerate(arrays)
-        ]
 
     plt.show()
 
@@ -157,8 +248,17 @@ def display_detector(detector, array="all"):
 # These method are used to display the detector memory
 
 
-def display_persist(persist_dict):
-    """Uses the meomry dict to plot all trapped charges"""
+def display_persist(persist_dict: dict) -> None:
+    """Plot all trapped charges using the memory dict.
+
+    Parameters
+    ----------
+    persist_dict: dict
+
+    Returns
+    -------
+    None
+    """
     trapped_charges = [trapmap for trapmap in persist_dict.values()]
 
     fig, axes = plt.subplots(
