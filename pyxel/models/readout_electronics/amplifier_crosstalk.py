449752dacad638b7af34fcd1984a791e8c03c432
Matej Arko <matej.arko@esa.int>
2020-12-03 15:41:22 +0100

    + CHG: Added documentation, mypy fixes

--- pyxel/models/readout_electronics/amplifier_crosstalk.py
+++ pyxel/models/readout_electronics/amplifier_crosstalk.py
@@ -4,44 +4,61 @@
 #  is part of this Pyxel package. No part of the package, including
 #  this file, may be copied, modified, propagated, or distributed except according to
 #  the terms contained in the file ‘LICENCE.txt’.
-#
-#
-#
-#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
-#  is part of this Pyxel package. No part of the package, including
-#  this file, may be copied, modified, propagated, or distributed except according to
-#  the terms contained in the file ‘LICENCE.txt’.
-#
-#
+
 """Amplifier crosstalk model: https://arxiv.org/abs/1808.00790."""
 
 import typing as t
+from pathlib import Path
 
 import numba
 import numpy as np
+
 from pyxel.inputs_outputs import load_table
-from pathlib import Path
+
 if t.TYPE_CHECKING:
     from pyxel.detectors import Detector
 
 
 @numba.njit
-def flip_array(array: np.ndarray, pos: int) -> np.ndarray:
-    """Flip the array for read direction as in starting position 1 or back."""
-    if pos == 1:
+def flip_array(array: np.ndarray, direction: int) -> np.ndarray:
+    """Flip the array for read direction as in case 1 or back.
+
+    Parameters
+    ----------
+    array: ndarray
+    direction: int
+
+    Returns
+    -------
+    ndarray
+    """
+    if direction == 1:
         return array
-    elif pos == 2:
+    elif direction == 2:
         return np.fliplr(array)
-    elif pos == 3:
+    elif direction == 3:
         return np.flipud(array)
-    elif pos == 4:
+    elif direction == 4:
         return np.fliplr(np.flipud(array))
     else:
         raise ValueError("Unknown readout direction.")
 
 
 @numba.njit
-def get_channel_slices(detector_shape: tuple, channel_matrix: np.array):
+def get_channel_slices(
+    detector_shape: tuple, channel_matrix: np.array
+) -> t.List[t.List[slice]]:
+    """Get pairs of slices that correspond to the given channel matrix in numerical order of channels.
+
+    Parameters
+    ----------
+    detector_shape: tuple
+    channel_matrix: ndarray
+
+    Returns
+    -------
+    slices
+    """
     columns = True
     j_x = channel_matrix.shape[0]
     j_y = 1
@@ -78,6 +95,16 @@ def get_channel_slices(detector_shape: tuple, channel_matrix: np.array):
 
 
 def get_matrix(coupling_matrix: t.Union[str, Path, list]) -> np.ndarray:
+    """Get the coupling matrix either from configuration input or a file.
+
+    Parameters
+    ----------
+    coupling_matrix
+
+    Returns
+    -------
+    ndarray
+    """
     if isinstance(coupling_matrix, list):
         return np.array(coupling_matrix)
     else:
@@ -90,8 +117,21 @@ def crosstalk_signal_ac(
     coupling_matrix: np.ndarray,
     channel_matrix: np.ndarray,
     readout_directions: np.ndarray,
-):
-    amp_number = channel_matrix.size   # number of amplifiers
+) -> np.ndarray:
+    """Apply AC crosstalk signal to an array.
+
+    Parameters
+    ----------
+    array: ndarray
+    coupling_matrix: ndarray
+    channel_matrix: ndarray
+    readout_directions: ndarray
+
+    Returns
+    -------
+    array: ndarray
+    """
+    amp_number = channel_matrix.size  # number of amplifiers
 
     slices = get_channel_slices(array.shape, channel_matrix)
 
@@ -100,10 +140,14 @@ def crosstalk_signal_ac(
     for k in range(amp_number):
         for j in range(amp_number):
             if k != j and coupling_matrix[k][j] != 0:
-                s_k = flip_array(array_copy[slices[k][1], slices[k][0]], readout_directions[k])
+                s_k = flip_array(
+                    array_copy[slices[k][1], slices[k][0]], readout_directions[k]
+                )
                 s_k_shift = np.hstack((s_k[:, 0:1], s_k[:, 0:-1]))
                 delta_s = s_k - s_k_shift
-                crosstalk_signal = coupling_matrix[k][j] * flip_array(delta_s, readout_directions[j])
+                crosstalk_signal = coupling_matrix[k][j] * flip_array(
+                    delta_s, readout_directions[j]
+                )
                 array[slices[j][1], slices[j][0]] += crosstalk_signal
 
     return array
@@ -115,7 +159,20 @@ def crosstalk_signal_dc(
     coupling_matrix: np.ndarray,
     channel_matrix: np.ndarray,
     readout_directions: np.ndarray,
-):
+) -> np.ndarray:
+    """Apply DC crosstalk signal to an array.
+
+    Parameters
+    ----------
+    array: ndarray
+    coupling_matrix: ndarray
+    channel_matrix: ndarray
+    readout_directions: ndarray
+
+    Returns
+    -------
+    array: ndarray
+    """
     amp_number = channel_matrix.size  # number of amplifiers
 
     slices = get_channel_slices(
@@ -127,54 +184,102 @@ def crosstalk_signal_dc(
     for k in range(amp_number):
         for j in range(amp_number):
             if k != j and coupling_matrix[k][j] != 0:
-                s_k = flip_array(array_copy[slices[k][1], slices[k][0]], readout_directions[k])
-                crosstalk_signal = coupling_matrix[k][j] * flip_array(s_k, readout_directions[j])
+                s_k = flip_array(
+                    array_copy[slices[k][1], slices[k][0]], readout_directions[k]
+                )
+                crosstalk_signal = coupling_matrix[k][j] * flip_array(
+                    s_k, readout_directions[j]
+                )
                 array[slices[j][1], slices[j][0]] += crosstalk_signal
 
     return array
 
 
 def dc_crosstalk(
-    detector: "Detector", coupling_matrix: t.Union[str, Path, list], channel_matrix: list, readout_directions: list
-):
-    coupling_matrix = get_matrix(coupling_matrix)  # type: np.ndarray
-    channel_matrix = np.array(channel_matrix)  # type: np.ndarray
-    readout_directions = np.array(readout_directions)  # type: np.ndarray
+    detector: "Detector",
+    coupling_matrix: t.Union[str, Path, list],
+    channel_matrix: list,
+    readout_directions: list,
+) -> None:
+    """Apply DC crosstalk signal to detector signal.
 
-    if detector.geometry.row % channel_matrix.shape[0] != 0:
-        raise ValueError("Can't split detector array horizontally for a given number of amplifiers.")
-    if len(channel_matrix.shape) > 1:
-        if detector.geometry.col % channel_matrix.shape[1] != 0:
-            raise ValueError("Can't split detector array vertically for a given number of amplifiers.")
-    if channel_matrix.size != readout_directions.size:
-        raise ValueError("Channel matrix and readout directions arrays not the same size.")
+    Parameters
+    ----------
+    detector: Detector
+    coupling_matrix: ndarray
+    channel_matrix: ndarray
+    readout_directions: ndarray
+
+    Returns
+    -------
+    None
+    """
+    cpl_matrix = get_matrix(coupling_matrix)  # type: np.ndarray
+    ch_matrix = np.array(channel_matrix)  # type: np.ndarray
+    directions = np.array(readout_directions)  # type: np.ndarray
+
+    if detector.geometry.row % ch_matrix.shape[0] != 0:
+        raise ValueError(
+            "Can't split detector array horizontally for a given number of amplifiers."
+        )
+    if len(ch_matrix.shape) > 1:
+        if detector.geometry.col % ch_matrix.shape[1] != 0:
+            raise ValueError(
+                "Can't split detector array vertically for a given number of amplifiers."
+            )
+    if ch_matrix.size != directions.size:
+        raise ValueError(
+            "Channel matrix and readout directions arrays not the same size."
+        )
 
     _ = crosstalk_signal_dc(
         array=detector.signal.array,
-        coupling_matrix=coupling_matrix,
-        channel_matrix=channel_matrix,
-        readout_directions=readout_directions,
+        coupling_matrix=cpl_matrix,
+        channel_matrix=ch_matrix,
+        readout_directions=directions,
     )
 
 
 def ac_crosstalk(
-    detector: "Detector", coupling_matrix: t.Union[str, Path, list], channel_matrix: list, readout_directions: list
-):
-    coupling_matrix = get_matrix(coupling_matrix)  # type: np.ndarray
-    channel_matrix = np.array(channel_matrix)  # type: np.ndarray
-    readout_directions = np.array(readout_directions)  # type: np.ndarray
+    detector: "Detector",
+    coupling_matrix: t.Union[str, Path, list],
+    channel_matrix: list,
+    readout_directions: list,
+) -> None:
+    """Apply AC crosstalk signal to detector signal.
 
-    if detector.geometry.row % channel_matrix.shape[0] != 0:
-        raise ValueError("Can't split detector array horizontally for a given number of amplifiers.")
-    if len(channel_matrix.shape) > 1:
-        if detector.geometry.col % channel_matrix.shape[1] != 0:
-            raise ValueError("Can't split detector array vertically for a given number of amplifiers.")
-    if channel_matrix.size != readout_directions.size:
-        raise ValueError("Channel matrix and readout directions arrays not the same size.")
+    Parameters
+    ----------
+    detector: Detector
+    coupling_matrix: ndarray
+    channel_matrix: ndarray
+    readout_directions: ndarray
+
+    Returns
+    -------
+    None
+    """
+    cpl_matrix = get_matrix(coupling_matrix)  # type: np.ndarray
+    ch_matrix = np.array(channel_matrix)  # type: np.ndarray
+    directions = np.array(readout_directions)  # type: np.ndarray
+
+    if detector.geometry.row % ch_matrix.shape[0] != 0:
+        raise ValueError(
+            "Can't split detector array horizontally for a given number of amplifiers."
+        )
+    if len(ch_matrix.shape) > 1:
+        if detector.geometry.col % ch_matrix.shape[1] != 0:
+            raise ValueError(
+                "Can't split detector array vertically for a given number of amplifiers."
+            )
+    if ch_matrix.size != directions.size:
+        raise ValueError(
+            "Channel matrix and readout directions arrays not the same size."
+        )
 
     _ = crosstalk_signal_ac(
         array=detector.signal.array,
-        coupling_matrix=coupling_matrix,
-        channel_matrix=channel_matrix,
-        readout_directions=readout_directions,
+        coupling_matrix=cpl_matrix,
+        channel_matrix=ch_matrix,
+        readout_directions=directions,
     )
