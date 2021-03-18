c8cafb9c68b011df477582174b2698444d1e50dd
Matej Arko <matej.arko@esa.int>
2021-03-18 12:49:49 +0100

    + CHG: Upgraded templates, created folder notebook

---
+++ pyxel/notebook/html_representation.py
@@ -0,0 +1,138 @@
+#  Copyright (c) European Space Agency, 2017, 2018, 2019, 2021.
+#
+#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#  is part of this Pyxel package. No part of the package, including
+#  this file, may be copied, modified, propagated, or distributed except according to
+#  the terms contained in the file ‘LICENCE.txt’.
+#
+#
+#
+#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#  is part of this Pyxel package. No part of the package, including
+#  this file, may be copied, modified, propagated, or distributed except according to
+#  the terms contained in the file ‘LICENCE.txt’.
+#
+#
+"""HTML display for Pyxel classes."""
+
+from jinja2 import Template
+from IPython.core.display import HTML
+import typing as t
+from pyxel.pipelines import ModelGroup, ModelFunction
+
+
+def display_html(obj: t.Callable) -> HTML:
+
+    if isinstance(obj, ModelGroup):
+        return display_model_group_html(obj)
+    elif isinstance(obj, ModelFunction):
+        return display_model_html(obj)
+    else:
+
+        d = {key: [type(obj.__dict__[key]).__name__, str(obj.__dict__[key]).replace(">", "&gt").replace("<", "&lt")] for key in obj.__dict__.keys()}
+
+        template_str = """
+        <h4>{{name}}</h4>
+        <table>
+        <tr>
+            <th> ATTRIBUTE </th>
+            <th> DTYPE </th>
+            <th> VALUE </td>
+        </tr>
+        {% for key, value in data.items() %}
+            <tr>
+                <th> {{ key }} </th>
+                <td> {{ value[0] }} </td>
+                <td> {{ value[1] }} </td>
+            </tr>
+        {% endfor %}
+        </table>"""
+
+        template = Template(template_str)
+
+        return HTML(template.render(data=d, name=obj.__class__.__name__))
+
+
+def display_model_html(mf: "ModelFunction") -> HTML:
+    d = {key: [type(mf.__dict__[key]).__name__, str(mf.__dict__[key]).replace(">", "&gt").replace("<", "&lt")] for key in mf.__dict__.keys()}
+    a = {key: [type(mf._arguments[key]).__name__, str(mf._arguments[key]).replace(">", "&gt").replace("<", "&lt")] for key in mf._arguments.keys()}
+
+    template_str = """
+    <h4>ModelFunction: {{model_name}}</h4>
+    <table>
+    <tr>
+        <th> ATTRIBUTE </th>
+        <th> DTYPE </th>
+        <th> VALUE </td>
+    </tr>
+    {% for key, value in data.items() %}
+        <tr>
+            <th> {{ key }} </th>
+            <td> {{ value[0] }} </td>
+            <td> {{ value[1] }} </td>
+        </tr>
+    {% endfor %}
+    </table>
+    <h5>Arguments</h5>
+    <table>
+    <tr>
+        <th> ATTRIBUTE </th>
+        <th> DTYPE </th>
+        <th> VALUE </td>
+    </tr>
+    {% for key, value in arguments.items() %}
+        <tr>
+            <th> {{ key }} </th>
+            <td> {{ value[0] }} </td>
+            <td> {{ value[1] }} </td>
+        </tr>
+    {% endfor %}
+    </table>
+    """
+
+    template = Template(template_str)
+
+    return HTML(template.render(data=d, arguments=a, model_name=mf._name))
+
+
+def display_model_group_html(mg: "ModelGroup") -> HTML:
+
+    d = {key: [type(mg.__dict__[key]).__name__, str(mg.__dict__[key]).replace(">", "&gt").replace("<", "&lt")] for key in mg.__dict__.keys()}
+    m = {model._name: [type(model).__name__,f"{model!r}".replace(">", "&gt").replace("<", "&lt")] for model in mg.models}
+
+    template_str = """
+    <h4>ModelGroup: {{model_group_name}}</h4>
+    <table>
+    <tr>
+        <th> ATTRIBUTE </th>
+        <th> DTYPE </th>
+        <th> VALUE </td>
+    </tr>
+    {% for key, value in data.items() %}
+        <tr>
+            <th> {{ key }} </th>
+            <td> {{ value[0] }} </td>
+            <td> {{ value[1] }} </td>
+        </tr>
+    {% endfor %}
+    </table>
+    <h5>Models</h5>
+    <table>
+    <tr>
+        <th> ATTRIBUTE </th>
+        <th> DTYPE </th>
+        <th> VALUE </td>
+    </tr>
+    {% for key, value in models.items() %}
+        <tr>
+            <th> {{ key }} </th>
+            <td> {{ value[0] }} </td>
+            <td> {{ value[1] }} </td>
+        </tr>
+    {% endfor %}
+    </table>
+    """
+
+    template = Template(template_str)
+
+    return HTML(template.render(data=d, models=m, model_group_name=mg._name))
