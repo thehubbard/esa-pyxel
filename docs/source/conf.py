b62c0640d8c7e3006ab589e55c210308bd6a7cf0
Frederic Lemmel <frederic.lemmel@esa.int>
2021-04-01 10:52:39 +0200

    + CHG: Add version in title.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -128,7 +128,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = "sphinx_book_theme"
-html_title = ""
+html_title = pyxel.__version__
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
