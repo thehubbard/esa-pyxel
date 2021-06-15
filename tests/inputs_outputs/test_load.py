f064db6322f49eb5264460776fe6303562394954
Frederic Lemmel <frederic.lemmel@esa.int>
2021-06-15 14:14:36 +0200

    + CHG: Add a new test calling function 'load' consecutively.

---
+++ tests/inputs_outputs/test_load.py
@@ -0,0 +1,33 @@
+#  Copyright (c) European Space Agency, 2017, 2018, 2019, 2020, 2021.
+#
+#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#  is part of this Pyxel package. No part of the package, including
+#  this file, may be copied, modified, propagated, or distributed except according to
+#  the terms contained in the file ‘LICENCE.txt’.
+
+import os
+from pathlib import Path
+
+from freezegun import freeze_time
+
+from pyxel.inputs_outputs import load
+
+
+def test_load_2_times(tmp_path: Path):
+    """Test function 'pyxel.inputs_outputs.load' called two times."""
+    filename = "../data/dummy_simple.yaml"
+
+    # Get full filename
+    full_filename = Path(__file__).parent / filename  # type: Path
+    assert full_filename.exists()
+
+    # Change working folder
+    os.chdir(tmp_path)
+
+    # Load the configuration file for the first time
+    with freeze_time("2021-06-15 14:11"):
+        _ = load(full_filename)
+
+    # Load the configuration file for the second time
+    with freeze_time("2021-06-15 14:11"):
+        _ = load(full_filename)
