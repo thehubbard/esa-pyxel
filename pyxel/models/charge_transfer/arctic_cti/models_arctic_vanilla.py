34f1d97c087548bb512a39fd6bb16f36b4c282ad
Frederic Lemmel <frederic.lemmel@esa.int>
2021-07-20 08:35:36 +0200

    + CHG: Create new models to add and to remove cti

---
+++ pyxel/models/charge_transfer/arctic_cti/models_arctic_vanilla.py
@@ -0,0 +1,103 @@
+#  Copyright (c) European Space Agency, 2017, 2018, 2019, 2020, 2021.
+#
+#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#  is part of this Pyxel package. No part of the package, including
+#  this file, may be copied, modified, propagated, or distributed except according to
+#  the terms contained in the file ‘LICENCE.txt’.
+#
+#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#  is part of this Pyxel package. No part of the package, including
+#  this file, may be copied, modified, propagated, or distributed except according to
+#  the terms contained in the file ‘LICENCE.txt’.
+
+import typing as t
+
+import arcticpy as ac
+import numpy as np
+
+from pyxel.detectors import CCD
+
+
+def _create_traps(instant_traps: t.Sequence[t.Mapping[str, float]]) -> t.List[ac.Trap]:
+    """Create traps
+
+    Parameters
+    ----------
+    instant_traps
+
+    Returns
+    -------
+
+    """
+
+
+def arctic_add(
+    detector: CCD,
+    well_fill_power: float,
+    instant_traps: t.Sequence[t.Mapping[str, float]],
+    express: int = 0,
+) -> None:
+
+    ccd = ac.CCD(
+        well_fill_power=well_fill_power, full_well_depth=detector.characteristics.fwc
+    )
+    roe = ac.ROE()
+
+    # Build the traps
+    traps = []  # type: t.List[ac.Trap]
+    for trap_info in instant_traps:
+        density = trap_info["density"]  # type: float
+        release_timescale = trap_info["release_timescale"]  # type: float
+
+        trap = ac.Trap(density=density, release_timescale=release_timescale)
+        traps.append(trap)
+
+    # Add CTI
+    image_2d = np.asarray(detector.pixel.array, dtype=float)  # type: np.ndarray
+
+    image_cti_added_2d = ac.add_cti(
+        image=image_2d,
+        parallel_traps=traps,
+        parallel_ccd=ccd,
+        parallel_roe=roe,
+        parallel_express=express,
+    )
+
+    detector.pixel.array = image_cti_added_2d
+
+
+def arctic_remove(
+    detector: CCD,
+    well_fill_power: float,
+    instant_traps: t.Sequence[t.Mapping[str, float]],
+    num_iterations: int,
+    express: int = 0,
+) -> None:
+
+    ccd = ac.CCD(
+        well_fill_power=well_fill_power, full_well_depth=detector.characteristics.fwc
+    )
+    roe = ac.ROE()
+
+    # Build the traps
+    traps = []  # type: t.List[ac.Trap]
+    for trap_info in instant_traps:
+        density = trap_info["density"]  # type: float
+        release_timescale = trap_info["release_timescale"]  # type: float
+
+        trap = ac.Trap(density=density, release_timescale=release_timescale)
+        traps.append(trap)
+
+    # Remove CTI
+    image_2d = np.asarray(detector.pixel.array, dtype=float)  # type: np.ndarray
+
+    image_cti_removed = ac.remove_cti(
+        image=image_2d,
+        iterations=num_iterations,
+        parallel_traps=traps,
+        parallel_ccd=ccd,
+        parallel_roe=roe,
+        parallel_express=express,
+    )
+
+    detector.pixel.array = image_cti_removed
---
+++ pyxel/models/charge_transfer/tests/test_arctic_add_traps.py
@@ -0,0 +1,77 @@
+#  Copyright (c) European Space Agency, 2017, 2018, 2019, 2020, 2021.
+#
+#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#  is part of this Pyxel package. No part of the package, including
+#  this file, may be copied, modified, propagated, or distributed except according to
+#  the terms contained in the file ‘LICENCE.txt’.
+
+import numpy as np
+
+from pyxel.detectors import CCD, CCDCharacteristics, CCDGeometry, Environment, Material
+from pyxel.models.charge_transfer.arctic_cti.models_arctic_numba import arctic_add_numba
+from pyxel.models.charge_transfer.arctic_cti.models_arctic_vanilla import arctic_add
+
+
+def create_detector() -> CCD:
+    """Create a valid `CCD` object."""
+    pixel_2d = np.zeros((100, 10), dtype=int)
+
+    pixel_2d[25:50, :] = 100
+    pixel_2d[75:, :] = 100
+
+    pixel_2d = pixel_2d[::5, ::5]
+
+    detector = CCD(
+        geometry=CCDGeometry(row=20, col=2),
+        material=Material(),
+        environment=Environment(),
+        characteristics=CCDCharacteristics(fwc=100_000),
+    )
+
+    detector.pixel.array = pixel_2d
+
+    return detector
+
+
+def test_add_one_trap():
+    """Test arctic model without numba."""
+
+    instant_traps = [{"density": 90.0, "release_timescale": 1.2}]
+
+    detector1 = create_detector()  # type: CCD
+    detector2 = create_detector()  # type: CCD
+
+    # Use library 'arcticpy'
+    arctic_add(detector=detector1, well_fill_power=0.8, instant_traps=instant_traps)
+
+    # Use optimized 'arcticpy'
+    arctic_add_numba(
+        detector=detector2, well_fill_power=0.8, instant_traps=instant_traps
+    )
+
+    np.testing.assert_almost_equal(detector1.pixel.array, detector2.pixel.array)
+
+
+def test_add_five_traps():
+    """Test arctic model without numba."""
+
+    instant_traps = [
+        {"density": 91.98081597, "release_timescale": 2.07392977},
+        {"density": 100.32049957, "release_timescale": 0.75635299},
+        {"density": 103.70445648, "release_timescale": 1.48364189},
+        {"density": 100.76309597, "release_timescale": 0.70015936},
+        {"density": 104.31871946, "release_timescale": 1.30312337},
+    ]
+
+    detector1 = create_detector()  # type: CCD
+    detector2 = create_detector()  # type: CCD
+
+    # Use library 'arcticpy'
+    arctic_add(detector=detector1, well_fill_power=0.8, instant_traps=instant_traps)
+
+    # Use optimized 'arcticpy'
+    arctic_add_numba(
+        detector=detector2, well_fill_power=0.8, instant_traps=instant_traps
+    )
+
+    np.testing.assert_almost_equal(detector1.pixel.array, detector2.pixel.array)
