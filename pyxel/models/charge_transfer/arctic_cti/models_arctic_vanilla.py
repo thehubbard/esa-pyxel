336e4c49c0db924e9052bc2b9197e87d6d420b49
Brad <braderz107@btinternet.com>
2021-08-11 17:18:35 +0100

    List functionality added and Traps replaced with TrapInstantCapture so latest arcitc version works

--- pyxel/models/charge_transfer/arctic_cti/models_arctic_vanilla.py
+++ pyxel/models/charge_transfer/arctic_cti/models_arctic_vanilla.py
@@ -18,7 +18,7 @@ import numpy as np
 from pyxel.detectors import CCD
 
 
-def _create_traps(instant_traps: t.Sequence[t.Mapping[str, float]]) -> t.List[ac.Trap]:
+def _create_traps(instant_traps: t.Sequence[t.Mapping[str, float]]) -> t.List[ac.TrapInstantCapture]:
     """Create traps
 
     Parameters
@@ -34,31 +34,41 @@ def _create_traps(instant_traps: t.Sequence[t.Mapping[str, float]]) -> t.List[ac
 def arctic_add(
     detector: CCD,
     well_fill_power: float,
-    # instant_traps: t.Sequence[t.Mapping[str, float]],
-    trap_1_density: float,
-    trap_1_release_time_scale: float,
+    trap_densities: list,
+    trap_release_timescales: list,
     express: int = 0,
+    # instant_traps: t.Sequence[t.Mapping[str, float]],
+
 ) -> None:
 
     ccd = ac.CCD(
-        well_fill_power=well_fill_power, full_well_depth=detector.characteristics.fwc
+        phases=[
+            ac.CCDPhase(
+                full_well_depth=detector.characteristics.fwc,
+                well_fill_power=well_fill_power,
+            )
+        ]
     )
+
     roe = ac.ROE()
 
-    instant_traps = [
-        {"density": trap_1_density, "release_timescale": trap_1_release_time_scale}
-    ]  # type: t.Sequence[t.Mapping[str, float]]
+    traps = []
+    for trap_density in trap_densities:
+        i = 0
+        instant_traps = [
+            {"density": trap_density, "release_timescale": trap_release_timescales[i]}
+        ]  # type: t.Sequence[t.Mapping[str, float]]
 
-    # Build the traps
-    traps = []  # type: t.List[ac.Trap]
-    for trap_info in instant_traps:
-        density = trap_info["density"]  # type: float
-        release_timescale = trap_info["release_timescale"]  # type: float
+        # Build the traps
+        # type: t.List[ac.TrapInstantCapture]
+        for trap_info in instant_traps:
+            density = trap_info["density"]  # type: float
+            release_timescale = trap_info["release_timescale"]  # type: float
+            trap = ac.TrapInstantCapture(density=density, release_timescale=release_timescale)
+            traps.append(trap)
+        # Add CTI
+        i+=1
 
-        trap = ac.Trap(density=density, release_timescale=release_timescale)
-        traps.append(trap)
-
-    # Add CTI
     image_2d = np.asarray(detector.pixel.array, dtype=float)  # type: np.ndarray
 
     image_cti_added_2d = ac.add_cti(
