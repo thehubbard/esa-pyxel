b1b48bf57872265795885bc2c89765bc8191d4c6
Frederic Lemmel <frederic.lemmel@esa.int>
2021-10-06 14:17:11 +0200

    Add unit tests for class `Geometry`.

--- tests/detectors/test_geometry.py
+++ tests/detectors/test_geometry.py
@@ -5,10 +5,11 @@
 #  this file, may be copied, modified, propagated, or distributed except according to
 #  the terms contained in the file ‘LICENCE.txt’.
 
+from copy import deepcopy
 import typing as t
 from dataclasses import dataclass
-
 import numpy as np
+
 import pytest
 
 from pyxel.detectors import CCDGeometry, CMOSGeometry, Geometry
@@ -101,3 +102,148 @@ def test_horizontal_pixel_center_pos(
     values = geometry.horizontal_pixel_center_pos_list()
 
     np.testing.assert_allclose(values, exp_values)
+
+
+@pytest.mark.parametrize("row", [0, 10000])
+@pytest.mark.parametrize("col", [0, 10000])
+@pytest.mark.parametrize("total_thickness", [0.0, 10000.0])
+@pytest.mark.parametrize("pixel_vert_size", [0.0, 1000.0])
+@pytest.mark.parametrize("pixel_horz_size", [0.0, 1000.0])
+def test_create_valid_geometry(
+    row, col, total_thickness, pixel_vert_size, pixel_horz_size
+):
+    """Test when creating a valid `Geometry` object."""
+    _ = Geometry(
+        row=row,
+        col=col,
+        total_thickness=total_thickness,
+        pixel_vert_size=pixel_vert_size,
+        pixel_horz_size=pixel_horz_size,
+    )
+
+
+@pytest.mark.parametrize(
+    "row, col, total_thickness, pixel_vert_size, pixel_horz_size, exp_exc",
+    [
+        pytest.param(-1, 100, 100.0, 100.0, 100.0, ValueError, id="row < 0"),
+        pytest.param(10001, 100, 100.0, 100.0, 100.0, ValueError, id="row > 10000"),
+        pytest.param(100, -1, 100.0, 100.0, 100.0, ValueError, id="col < 0"),
+        pytest.param(100, 10001, 100.0, 100.0, 100.0, ValueError, id="col > 10000"),
+        pytest.param(
+            100, 100, -0.1, 100.0, 100.0, ValueError, id="total_thickness < 0."
+        ),
+        pytest.param(
+            100, 100, 10000.1, 100.0, 100.0, ValueError, id="total_thickness > 10000."
+        ),
+    ],
+)
+def test_create_invalid_geometry(
+    row, col, total_thickness, pixel_vert_size, pixel_horz_size, exp_exc
+):
+    """Test when creating an invalid `Geometry` object."""
+    with pytest.raises(exp_exc):
+        _ = Geometry(
+            row=row,
+            col=col,
+            total_thickness=total_thickness,
+            pixel_vert_size=pixel_vert_size,
+            pixel_horz_size=pixel_horz_size,
+        )
+
+
+@pytest.mark.parametrize(
+    "other_obj, is_equal",
+    [
+        pytest.param(None, False, id="None"),
+        pytest.param(Geometry(), False, id="Empty 'Geometry'"),
+        pytest.param(Geometry(row=100), False, id="Only one parameter"),
+        pytest.param(
+            Geometry(
+                row=100,
+                col=120,
+                total_thickness=123.1,
+                pixel_horz_size=12.4,
+                pixel_vert_size=34.5,
+            ),
+            True,
+            id="Same parameters, same class",
+        ),
+        pytest.param(
+            CCDGeometry(
+                row=100,
+                col=120,
+                total_thickness=123.1,
+                pixel_horz_size=12.4,
+                pixel_vert_size=34.5,
+            ),
+            False,
+            id="Same parameters, different class",
+        ),
+    ],
+)
+def test_is_equal(other_obj, is_equal):
+    """Test equality statement for Geometry."""
+    obj = Geometry(
+        row=100,
+        col=120,
+        total_thickness=123.1,
+        pixel_horz_size=12.4,
+        pixel_vert_size=34.5,
+    )
+
+    if is_equal:
+        assert obj == other_obj
+    else:
+        assert obj != other_obj
+
+
+@pytest.mark.parametrize(
+    "obj, exp_dict",
+    [
+        (
+            Geometry(),
+            {
+                "row": 0,
+                "col": 0,
+                "total_thickness": 0.0,
+                "pixel_vert_size": 0.0,
+                "pixel_horz_size": 0.0,
+            },
+        ),
+        (
+            Geometry(
+                row=100,
+                col=120,
+                total_thickness=123.1,
+                pixel_horz_size=12.4,
+                pixel_vert_size=34.5,
+            ),
+            {
+                "row": 100,
+                "col": 120,
+                "total_thickness": 123.1,
+                "pixel_horz_size": 12.4,
+                "pixel_vert_size": 34.5,
+            },
+        ),
+    ],
+)
+def test_to_and_from_dict(obj, exp_dict):
+    """Test methods 'to_dict', 'from_dict'."""
+    assert isinstance(obj, Geometry)
+
+    # Convert from `Geometry` to a `dict`
+    dct = obj.to_dict()
+    assert dct == exp_dict
+
+    # Convert from `dict` to `Geometry`
+    copied_dct = deepcopy(exp_dict)  # create a new dict
+    assert copied_dct is not exp_dict
+    assert copied_dct == exp_dict
+
+    other_obj = Geometry.from_dict(copied_dct)
+    assert isinstance(other_obj, Geometry)
+
+    assert obj == other_obj
+    assert obj is not other_obj
+    assert copied_dct == exp_dict
