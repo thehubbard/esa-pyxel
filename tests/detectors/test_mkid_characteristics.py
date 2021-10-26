0f107c7c7b45951357bb63042f9a00797c039230
Frederic Lemmel <frederic.lemmel@esa.int>
2021-10-26 15:25:29 +0200

    Add new unit tests 'test_mkid_characteristics'.

---
+++ tests/detectors/test_mkid_characteristics.py
@@ -0,0 +1,196 @@
+#  Copyright (c) European Space Agency, 2017, 2018, 2019, 2020, 2021.
+#
+#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
+#  is part of this Pyxel package. No part of the package, including
+#  this file, may be copied, modified, propagated, or distributed except according to
+#  the terms contained in the file ‘LICENCE.txt’.
+
+from copy import deepcopy
+
+import pytest
+
+from pyxel.detectors import Characteristics, MKIDCharacteristics
+
+
+@pytest.mark.parametrize(
+    "qe, eta, sv, amp, a1, a2, fwc, vg, dt, cutoff, vbiaspower, dsub, vreset, biasgate, preampref",
+    [
+        (0, 0, 0, 0, 0, 0, 0, 0.0, 0, 1.7, 0.0, 0.3, 0.0, 1.8, 0.0),
+        (
+            1.0,
+            1.0,
+            100.0,
+            100.0,
+            100.0,
+            65536,
+            10_000_000,
+            1.0,
+            10.0,
+            15.0,
+            3.4,
+            1.0,
+            0.3,
+            2.6,
+            4.0,
+        ),
+    ],
+)
+def test_create_valid_MKIDCharacteristics(
+    qe,
+    eta,
+    sv,
+    amp,
+    a1,
+    a2,
+    fwc,
+    vg,
+    dt,
+    cutoff,
+    vbiaspower,
+    dsub,
+    vreset,
+    biasgate,
+    preampref,
+):
+    """Test when creating a valid `MKIDCharacteristics` object."""
+    _ = MKIDCharacteristics(
+        qe=qe,
+        eta=eta,
+        sv=sv,
+        amp=amp,
+        a1=a1,
+        a2=a2,
+        fwc=fwc,
+        vg=vg,
+        dt=dt,
+        cutoff=cutoff,
+        vbiaspower=vbiaspower,
+        dsub=dsub,
+        vreset=vreset,
+        biasgate=biasgate,
+        preampref=preampref,
+    )
+
+
+@pytest.mark.parametrize(
+    "other_obj, is_equal",
+    [
+        pytest.param(None, False, id="None"),
+        pytest.param(MKIDCharacteristics(), False, id="Empty 'MKIDCharacteristics'"),
+        pytest.param(MKIDCharacteristics(qe=0.1), False, id="Only one parameter"),
+        pytest.param(
+            Characteristics(
+                qe=0.1, eta=0.2, sv=3.3, amp=4.4, a1=5, a2=6, fwc=7, vg=0.8, dt=9.9
+            ),
+            False,
+            id="Wrong type",
+        ),
+        pytest.param(
+            MKIDCharacteristics(
+                qe=0.1,
+                eta=0.2,
+                sv=3.3,
+                amp=4.4,
+                a1=5,
+                a2=6,
+                fwc=7,
+                vg=0.8,
+                dt=9.9,
+                cutoff=10.0,
+                vbiaspower=1.0,
+                dsub=0.5,
+                vreset=0.2,
+                biasgate=2.0,
+                preampref=3.0,
+            ),
+            True,
+            id="Almost same parameters, different class",
+        ),
+    ],
+)
+def test_is_equal(other_obj, is_equal):
+    """Test equality statement for `MKIDCharacteristics`."""
+    obj = MKIDCharacteristics(
+        qe=0.1,
+        eta=0.2,
+        sv=3.3,
+        amp=4.4,
+        a1=5,
+        a2=6,
+        fwc=7,
+        vg=0.8,
+        dt=9.9,
+        cutoff=10.0,
+        vbiaspower=1.0,
+        dsub=0.5,
+        vreset=0.2,
+        biasgate=2.0,
+        preampref=3.0,
+    )
+
+    if is_equal:
+        assert obj == other_obj
+    else:
+        assert obj != other_obj
+
+
+@pytest.mark.parametrize(
+    "obj, exp_dict",
+    [
+        (
+            MKIDCharacteristics(
+                qe=0.1,
+                eta=0.2,
+                sv=3.3,
+                amp=4.4,
+                a1=5,
+                a2=6,
+                fwc=7,
+                vg=0.8,
+                dt=9.9,
+                cutoff=10.0,
+                vbiaspower=1.0,
+                dsub=0.5,
+                vreset=0.2,
+                biasgate=2.0,
+                preampref=3.0,
+            ),
+            {
+                "qe": 0.1,
+                "eta": 0.2,
+                "sv": 3.3,
+                "amp": 4.4,
+                "a1": 5,
+                "a2": 6,
+                "fwc": 7,
+                "vg": 0.8,
+                "dt": 9.9,
+                "cutoff": 10.0,
+                "vbiaspower": 1.0,
+                "dsub": 0.5,
+                "vreset": 0.2,
+                "biasgate": 2.0,
+                "preampref": 3.0,
+            },
+        )
+    ],
+)
+def test_to_and_from_dict(obj, exp_dict):
+    """Test methods 'to_dict', 'from_dict'."""
+    assert type(obj) == MKIDCharacteristics
+
+    # Convert from `MKIDCharacteristics` to a `dict`
+    dct = obj.to_dict()
+    assert dct == exp_dict
+
+    # Copy 'exp_dict'
+    copied_dct = deepcopy(exp_dict)  # create a new dict
+    assert copied_dct is not exp_dict
+    assert copied_dct == exp_dict
+
+    # Convert from `dict` to `MKIDCharacteristics`
+    other_obj = MKIDCharacteristics.from_dict(copied_dct)
+    assert type(other_obj) == MKIDCharacteristics
+    assert obj == other_obj
+    assert obj is not other_obj
+    assert copied_dct == exp_dict
