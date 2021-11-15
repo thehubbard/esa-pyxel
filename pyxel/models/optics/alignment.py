e55b4a628f8bccb3930758aa3fe9d7cbdef1108f
Frederic Lemmel <frederic.lemmel@esa.int>
2021-11-15 08:19:09 +0100

    Refactoring model 'alignment'.

--- pyxel/models/optics/alignment.py
+++ pyxel/models/optics/alignment.py
@@ -6,26 +6,56 @@
 #  the terms contained in the file ‘LICENCE.txt’.
 
 """Pyxel photon generator models."""
+import numpy as np
+
 from pyxel.detectors import Detector
 
 
-# @pyxel.validate
-# @pyxel.argument(name='image_file', label='', validate=check_path)
-def alignment(detector: Detector) -> None:
-    """Optical alignment.
-
-    :param detector: Pyxel Detector object
+def apply_alignment(
+    data_2d: np.ndarray,
+    target_rows: int,
+    target_cols: int,
+) -> np.ndarray:
     """
-    geo = detector.geometry
-    rows, cols = detector.photon.array.shape
-    row0 = int((rows - geo.row) / 2)
-    col0 = int((cols - geo.col) / 2)
+
+    Parameters
+    ----------
+    data_2d : array
+    target_rows : int
+    target_cols : int
+
+    Returns
+    -------
+    array
+        An aligned 2D array.
+    """
+    rows, cols = data_2d.shape
+
+    row0 = int((rows - target_rows) / 2)
+    col0 = int((cols - target_cols) / 2)
 
     if row0 < 0 or col0 < 0:
         raise ValueError
 
-    aligned_optical_image = detector.photon.array[
-        slice(row0, row0 + geo.row), slice(col0, col0 + geo.col)
-    ]
+    aligned_data_2d = data_2d[slice(row0, row0 + rows), slice(col0, col0 + cols)]
+
+    return aligned_data_2d
+
+
+def alignment(detector: Detector) -> None:
+    """Optical alignment.
+
+    Parameters
+    ----------
+    detector: Detector
+        Pyxel Detector object.
+    """
+    geo = detector.geometry
+
+    aligned_optical_image = apply_alignment(
+        data_2d=detector.photon.array,
+        target_rows=geo.row,
+        target_cols=geo.col,
+    )
 
     detector.photon.array = aligned_optical_image
