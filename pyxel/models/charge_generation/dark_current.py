0bf89e434ba1dbd190e4a98c861af609d7e6deb2
thibaut prodhomme <thibaut.prodhomme@esa.int>
2021-11-18 16:38:07 +0100

    fixed dark current rule 07 model

--- pyxel/models/charge_generation/dark_current.py
+++ pyxel/models/charge_generation/dark_current.py
@@ -5,24 +5,75 @@
 #  this file, may be copied, modified, propagated, or distributed except according to
 #  the terms contained in the file ‘LICENCE.txt’.
 
-"""Simple model to generate charges due to dark current process."""
+"""Simple models to generate charge due to dark current process."""
 import logging
-import typing as t
 
 import numpy as np
 
 from pyxel.detectors import CMOS
 from pyxel.util import temporary_random_state
+import typing as t
 
-# TODO: more documentation, refactoring, random, astropy constants
+def lambda_e(lambda_cutoff: float) -> float:
+    """Compute lambda_e.
+
+    :param lambda_cutoff: (int) Cut-off wavelength of the detector
+    """
+    lambda_scale = 0.200847413  # um
+    lambda_threshold = 4.635136423  # um
+    pwr = 0.544071282
+    if lambda_cutoff < lambda_threshold:
+        le = lambda_cutoff / (
+                1
+                - ((lambda_scale / lambda_cutoff) - (lambda_scale / lambda_threshold))
+                ** pwr
+        )
+    else:
+        le = lambda_cutoff
+    return le
 
 
-# TODO: Fix this
-# @validators.validate
-# @config.argument(name='detector', label='', units='', validate=checkers.check_type(CMOS))
+def compute_mct_dark_rule07(
+        pitch: float, temperature: float, cut_off: float
+) -> float:
+    """
+
+    Parameters
+    ----------
+    pitch: float
+        the x and y dimension of the pixel in micrometer
+    temperature: float
+        the devices temperature
+    cut_off: float
+        the detector wavelength cut-off in micrometer
+
+    Returns
+    -------
+    dark_current: float
+        in e-/pixel/s
+    """
+    C = 1.16  # activation energy factor
+    Q = 1.602e-19  # Charge of one electron, unit= A.s
+    K = 1.3806504e-23  # Constant of Boltzmann, unit = m2.kg/s2/K
+
+    j0 = 8367  # obtained through fit of experimental data, see paper
+
+    e_c = 1.24 / lambda_e(cut_off)  # [eV] the optical gap extracted from the mid response cutoff
+
+    rule07 = j0 * np.exp(-C * Q * e_c / (K * temperature))  # A/cm3
+
+    amp_to_eps = 6.242e18  # e-/s
+    um2_to_cm2 = 1.0e-8
+    factor = amp_to_eps * pitch * pitch * um2_to_cm2  # to convert Amp/cm2 in e/pixel/s
+    dc = rule07 * factor
+
+    return dc
+
+
 @temporary_random_state
 def dark_current_rule07(detector: CMOS, seed: t.Optional[int] = None) -> None:
-    """Generate charge from dark current process.
+    """Generate charge from dark current process based on Rule07 paper by W.E. Tennant
+    Journal of Electronic Materials volume 37, pages1406–1410 (2008)
 
     Parameters
     ----------
@@ -32,42 +83,14 @@ def dark_current_rule07(detector: CMOS, seed: t.Optional[int] = None) -> None:
     # TODO: investigate on the knee of rule07 for higher 1/le*T values
     logging.info("")
     geo = detector.geometry
+
+    pitch = geo.pixel_vert_size  # assumes a square pitch
     temperature = detector.environment.temperature
-    cutoff = detector.characteristics.cutoff
+    cut_off = detector.characteristics.cutoff
 
-    amp_to_eps = 6.242e18  # conversion factor from Ampere to Electrons per second
-    um2_to_cm2 = 1.0e-8
-    conversion_factor = amp_to_eps * um2_to_cm2
+    dc = compute_mct_dark_rule07(pitch, temperature, cut_off)
 
-    # pitch = 18              # um
-    # Rule 07 empirical model parameters
-    j0 = 8367.000019  # A/cm**2
-    c = -1.162972237
-    q = 1.602176624e-19  # Elementary charge (Coulomb)
-    k = 1.38064852e-23  # Boltzmann constant (m2 kg s-2 K-1)
-
-    def lambda_e(lambda_cutoff: float) -> float:
-        """Compute lambda_e.
-
-        :param lambda_cutoff: (int) Cut-off wavelength of the detector
-        """
-        lambda_scale = 0.200847413  # um
-        lambda_threshold = 4.635136423  # um
-        pwr = 0.544071282
-        if lambda_cutoff < lambda_threshold:
-            le = lambda_cutoff / (
-                1
-                - ((lambda_scale / lambda_cutoff) - (lambda_scale / lambda_threshold))
-                ** pwr
-            )
-        else:
-            le = lambda_cutoff
-        return le
-
-    # Rule07
-    j = j0 * np.exp(c * (1.24 * q / k) * 1.0 / (lambda_e(cutoff) * temperature))
-    dark = j * conversion_factor * geo.pixel_horz_size * geo.pixel_vert_size
     # The number of charge generated with Poisson distribution using rule07 empiric law for lambda
-    charge_number = np.random.poisson(dark, size=(geo.row, geo.col))
+    charge_number = np.random.poisson(dc, size=(geo.row, geo.col))
 
     detector.charge.add_charge_array(charge_number)
