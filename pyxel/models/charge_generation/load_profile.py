4964c9442f79edd7a2dd5dbbee487df7d0bd64b7
Matej Arko <matej.arko@esa.int>
2021-11-30 16:27:59 +0100

    renaming in charge_profile

--- pyxel/models/charge_generation/load_profile.py
+++ pyxel/models/charge_generation/load_profile.py
@@ -22,7 +22,7 @@ from pyxel.inputs import load_image
 def load_charge_from_file(
     num_rows: int,
     num_cols: int,
-    txt_file: t.Union[str, Path],
+    filename: t.Union[str, Path],
     profile_position_y: int,
     profile_position_x: int,
     fit_profile_to_det: bool = False,
@@ -33,7 +33,7 @@ def load_charge_from_file(
     ----------
     num_rows: int
     num_cols: int
-    txt_file: str or Path
+    filename: str or Path
     profile_position_y: int
     profile_position_x: x
     fit_profile_to_det: bool
@@ -47,7 +47,7 @@ def load_charge_from_file(
 
     # Load 2d charge profile (which can be smaller or
     #                         larger in dimensions than detector imaging area)
-    charges_from_file_2d = load_image(txt_file)
+    charges_from_file_2d = load_image(filename)
 
     if fit_profile_to_det:
         # Crop 2d charge profile, so it is not larger in dimensions than detector imaging area)
@@ -67,9 +67,9 @@ def load_charge_from_file(
 
 def charge_profile(
     detector: Detector,
-    txt_file: t.Union[str, Path],
+    filename: t.Union[str, Path],
     fit_profile_to_det: bool = False,
-    profile_position: t.Optional[t.Tuple[int, int]] = None,
+    profile_position: t.Tuple[int, int] = (0, 0),
     time_scale: float = 1.0,
 ) -> None:
     """Load charge profile from txt file for detector, mostly for but not limited to CCDs.
@@ -78,18 +78,14 @@ def charge_profile(
     ----------
     detector : Detector
         Pyxel Detector object.
-    txt_file : str or Path
+    filename : str or Path
         File path.
     fit_profile_to_det : bool
     profile_position : list
     time_scale: float
         Time scale of the input charge, default is 1 second. 0.001 would be ms.
     """
-    if profile_position is None:
-        profile_position_y = 0  # type: int
-        profile_position_x = 0  # type: int
-    else:
-        profile_position_y, profile_position_x = profile_position
+    profile_position_y, profile_position_x = profile_position
 
     geo = detector.geometry  # type: Geometry
 
@@ -97,7 +93,7 @@ def charge_profile(
     charges = load_charge_from_file(
         num_rows=geo.row,
         num_cols=geo.col,
-        txt_file=txt_file,
+        filename=filename,
         profile_position_y=profile_position_y,
         profile_position_x=profile_position_x,
         fit_profile_to_det=fit_profile_to_det,
