fdbde9163b1200e111b5d44974e9fa35f27bcd74
Matej Arko <matej.arko@esa.int>
2021-12-09 17:00:23 +0100

    Removed custom_qe model

--- pyxel/models/charge_generation/__init__.py
+++ pyxel/models/charge_generation/__init__.py
@@ -13,5 +13,4 @@ from .dark_current_rule07 import dark_current_rule07
 from .load_charge import load_charge
 from .photoelectrons import simple_conversion, conversion_with_qe_map
 from .tars.tars import run_tars
-from .qe_map import qe_map
 from .dark_current import dark_current
