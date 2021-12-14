1710a24fe7bd8d14a708e04f56b063e670ac980c
Frederic Lemmel <frederic.lemmel@esa.int>
2021-12-14 17:49:20 +0100

    Display current version in the documentation.
    (cherry picked from commit 107313b7792302ef962585d8e67c1621a1ed4e18)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -145,7 +145,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = "sphinx_book_theme"
-html_title = "Pyxel documentation " + tag
+html_title = f"version {version}"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
