e9a9edab241b06f45bb0f0f1b96f036b16049cbf
Matej Arko <matej.arko@esa.int>
2022-01-14 12:18:53 +0100

    Removed unused properties from Environment class

--- tests/unittests/test_environment.py
+++ tests/unittests/test_environment.py
@@ -18,9 +18,9 @@ from pyxel.detectors import Environment
         # (1, None, None, 1.0, None, None),
         # (None, 2, None, None, 2.0, None),
         # (None, None, 3, None, None, 3.0),
-        ([1, 2, 3], [1.0, 2.0, 3.0]),
-        ([4.0, 5.0, 6.0], [4.0, 5.0, 6.0]),
-        ([7, 0, 0], [7.0, 0.0, 0.0]),
+        ([1], [1.0]),
+        ([4.0], [4.0]),
+        ([7], [7.0]),
     ],
 )
 def test_init(list_in, list_out):
@@ -35,8 +35,6 @@ def test_init(list_in, list_out):
 
     # Test getters
     assert obj.temperature == list_out[0]
-    assert obj.total_ionising_dose == list_out[1]
-    assert obj.total_non_ionising_dose == list_out[2]
 
 
 # @pytest.mark.parametrize("temperature, total_inonising_dose, total_non_ionising_dose, exp_exception, exp_msg", [
