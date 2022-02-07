36b5faafe593577b54b61eac6666904ebdca0d6a
Matej Arko <matej.arko@esa.int>
2022-02-07 11:49:36 +0100

    Fix in test_fitness

--- tests/unittests/test_fitting.py
+++ tests/unittests/test_fitting.py
@@ -297,7 +297,7 @@ def test_custom_fitness(yaml, simulated, target, weighting):
                     100.0,
                 ]
             ),
-            88453.49014776475,
+            88455.49014776475,
         )
     ],
 )
