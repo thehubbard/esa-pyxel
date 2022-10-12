81bd60e9856860de6919522e1ec683b93abcf859
thibaut prodhomme <thibaut.prodhomme@esa.int>
2022-10-12 16:42:41 +0200

    Modified the style to pass checks

--- pyxel/models/charge_generation/charge_deposition.py
+++ pyxel/models/charge_generation/charge_deposition.py
@@ -8,35 +8,35 @@
 """Simple models to simulate charge deposition by ionizing particles (e.g. cosmic rays)."""
 
 import typing as t
-
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
 from typing_extensions import Literal
 
-from pyxel.detectors import Detector
 from pyxel.data_structure import Charge
-from pyxel.util import temporary_random_state
-from pyxel.util import materials
+from pyxel.detectors import Detector
+from pyxel.util import materials, temporary_random_state
 
 
 @temporary_random_state
 def charge_deposition(
     detector: Detector,
     flux: float,
-    step_size: float = 1.,
-    energy_mean: float = 1.,
-    energy_spread: float = .1,
+    step_size: float = 1.0,
+    energy_mean: float = 1.0,
+    energy_spread: float = 0.1,
     energy_spectrum: t.Union[str, Path, None] = None,
     energy_spectrum_sampling: t.Optional[Literal["linear", "log", None]] = "log",
     ehpair_creation: float = 3.65,
     material_density: float = 2.3290,
-    particle_direction: t.Optional[Literal["isotropic", "orthogonal", None]] = "isotropic",
+    particle_direction: t.Optional[
+        Literal["isotropic", "orthogonal", None]
+    ] = "isotropic",
     stopping_power_curve: t.Union[str, Path, None] = None,
-    seed: t.Optional[int] = None
+    seed: t.Optional[int] = None,
 ) -> None:
-    """Simulate charge deposition by ionizing particles using a stopping power curve
+    """Simulate charge deposition by ionizing particles using a stopping power curve.
 
     Parameters
     ----------
@@ -89,7 +89,7 @@ def charge_deposition(
         ehpair_creation=ehpair_creation,
         material_density=material_density,
         particle_direction=particle_direction,
-        stopping_power_curve=stopping_power_curve
+        stopping_power_curve=stopping_power_curve,
     )
 
     detector.charge.add_charge_dataframe(tracks_to_charge(tracks))
@@ -99,17 +99,19 @@ def charge_deposition(
 def charge_deposition_in_mct(
     detector: Detector,
     flux: float,
-    step_size: float = 1.,
-    energy_mean: float = 1.,
-    energy_spread: float = .1,
+    step_size: float = 1.0,
+    energy_mean: float = 1.0,
+    energy_spread: float = 0.1,
     energy_spectrum: t.Union[str, Path, None] = None,
     energy_spectrum_sampling: t.Optional[Literal["linear", "log", None]] = "log",
     cutoff_wavelength: float = 2.5,
-    particle_direction: t.Optional[Literal["isotropic", "orthogonal", None]] = "isotropic",
+    particle_direction: t.Optional[
+        Literal["isotropic", "orthogonal", None]
+    ] = "isotropic",
     stopping_power_curve: t.Union[str, Path, None] = None,
-    seed: t.Optional[int] = None
+    seed: t.Optional[int] = None,
 ) -> None:
-    """Simulate charge deposition by ionizing particles using a stopping power curve
+    """Simulate charge deposition by ionizing particles using a stopping power curve.
 
     Parameters
     ----------
@@ -146,8 +148,8 @@ def charge_deposition_in_mct(
     seed: int, optional
     """
     lambdae = materials.lambda_e(cutoff_wavelength)
-    eg = 1.24/lambdae
-    ehpair_creation = 3*eg
+    eg = 1.24 / lambdae
+    ehpair_creation = 3 * eg
     x = materials.eg_hansen_inverse(eg, detector.environment.temperature)
     material_density = materials.density(x)
     tracks = simulate_charge_deposition(
@@ -164,29 +166,28 @@ def charge_deposition_in_mct(
         ehpair_creation=ehpair_creation,
         material_density=material_density,
         particle_direction=particle_direction,
-        stopping_power_curve=stopping_power_curve
+        stopping_power_curve=stopping_power_curve,
     )
 
     detector.charge.add_charge_dataframe(tracks_to_charge(tracks))
 
 
-def tracks_to_charge(tracks: list) -> pd.DataFrame:
-    """
-    Convert tracks into a panda dataframe compatible with a detector charge object.
+def tracks_to_charge(tracks: t.Sequence[float]) -> pd.DataFrame:
+    """Convert tracks into a panda dataframe compatible with a detector charge object.
 
-        Parameters
-        ----------
-        tracks : list
-            with the following structure for each track :
-            tracks[track_id], track_id = (vc, vx, vy, vz), vc[cluster_id] etc.
+    Parameters
+    ----------
+    tracks : list
+        with the following structure for each track :
+        tracks[track_id], track_id = (vc, vx, vy, vz), vc[cluster_id] etc.
     """
-    tracks = np.array(tracks, dtype='object')
+    # tracks = np.array(tracks, dtype="object")
 
-    tracks_arr = np.array(tracks, dtype='object')
+    tracks_arr = np.array(tracks, dtype="object")
     vc_arr = np.concatenate(tracks_arr[:, 0])
-    vx_arr = np.concatenate(tracks[:, 1])
-    vy_arr = np.concatenate(tracks[:, 2])
-    vz_arr = np.concatenate(tracks[:, 3])
+    vx_arr = np.concatenate(tracks_arr[:, 1])
+    vy_arr = np.concatenate(tracks_arr[:, 2])
+    vz_arr = np.concatenate(tracks_arr[:, 3])
     zeros = np.zeros(np.shape(vc_arr))
 
     return Charge.create_charges(
@@ -198,69 +199,72 @@ def tracks_to_charge(tracks: list) -> pd.DataFrame:
         init_z_position=vz_arr,
         init_ver_velocity=zeros,
         init_hor_velocity=zeros,
-        init_z_velocity=zeros)
+        init_z_velocity=zeros,
+    )
 
 
 def simulate_charge_deposition(
-        flux: float,
-        exposure: float,
-        x_lim: float,
-        y_lim: float,
-        z_lim: float,
-        step_size: float = 1.,
-        energy_mean: float = 1.,
-        energy_spread: float = .1,
-        energy_spectrum: t.Union[str, Path, None] = None,
-        energy_spectrum_sampling: t.Optional[Literal["linear", "log", None]] = "log",
-        ehpair_creation: float = 3.65,
-        material_density: float = 2.3290,
-        particle_direction: t.Optional[Literal["isotropic", "orthogonal", None]] = "isotropic",
-        stopping_power_curve: t.Union[str, Path, None] = None
+    flux: float,
+    exposure: float,
+    x_lim: float,
+    y_lim: float,
+    z_lim: float,
+    step_size: float = 1.0,
+    energy_mean: float = 1.0,
+    energy_spread: float = 0.1,
+    energy_spectrum: t.Union[str, Path, None] = None,
+    energy_spectrum_sampling: t.Optional[Literal["linear", "log", None]] = "log",
+    ehpair_creation: float = 3.65,
+    material_density: float = 2.3290,
+    particle_direction: t.Optional[
+        Literal["isotropic", "orthogonal", None]
+    ] = "isotropic",
+    stopping_power_curve: t.Union[str, Path, None] = None,
 ) -> list:
     """Simulate charge deposition of incident ionizing particles inside a detector.
 
-        Parameters
-        ----------
-        flux : float
-            the flux of incoming particles in particle/s
-        exposure : float
-            the detector exposure duration in s (how long it takes to take one readout)
-        x_lim : float
-            the maximum dimension of the sensor in the x direction
-        y_lim : float
-            the maximum dimension of the sensor in the y direction
-        z_lim : float
-            the maximum dimension of the sensor in the z direction (thickness)
-        step_size : float
-            the size of the considered unitary step in unit length along which energy is deposited
-        energy_mean : float
-            the mean energy of the incoming ionizing particles
-        energy_spread : float
-            the spread in energy of the incoming ionizing particles
-        energy_spectrum: String
-            the location of the file describing the energy spectrum of incident particles
-            if no spectrum is provided energies are randomly drawn from a normal distribution
-            with mean and spread defined above
-            note that the energy spectrum is assumed to be a txt file with two columns [energy, flux]
-            with the energy in MeV
-        energy_spectrum_sampling: String
-            "log" or None: the energy spectrum is sampled in log space
-            "linear" : the energy spectrum is sampled in linear space
-        ehpair_creation: float
-            the energy required to generate a electron-hole pair in eV
-            by default the Si value at room temperature is parsed i.e. 3.65 eV
-        material_density: float
-            the material density in g/cm3
-            by default he Si value at room temperature is parsed i.e. 2.3290 g/cm3
-        particle_direction: string
-            "isotropic" : particles are coming from all directions (outside of the sensor)
-            "orthogonal" : particles are coming from the top of the sensor (thickness = 0) and orthogonal to its surface
-        stopping_power_curve : String
-            the location of the file describing the total massive stopping power
-            energetic loss per mass of material and per unit path length versus particle energy
-            note that the the stopping power curve is assumed to be a csv file with two columns [energy, stopping power]
-            energy in MeV, stopping power in MeV cm2/g
-        """
+    Parameters
+    ----------
+    flux : float
+        the flux of incoming particles in particle/s
+    exposure : float
+        the detector exposure duration in s (how long it takes to take one readout)
+    x_lim : float
+        the maximum dimension of the sensor in the x direction
+    y_lim : float
+        the maximum dimension of the sensor in the y direction
+    z_lim : float
+        the maximum dimension of the sensor in the z direction (thickness)
+    step_size : float
+        the size of the considered unitary step in unit length along which energy is deposited
+    energy_mean : float
+        the mean energy of the incoming ionizing particles
+    energy_spread : float
+        the spread in energy of the incoming ionizing particles
+    energy_spectrum: String
+        the location of the file describing the energy spectrum of incident particles
+        if no spectrum is provided energies are randomly drawn from a normal distribution
+        with mean and spread defined above
+        note that the energy spectrum is assumed to be a txt file with two columns [energy, flux]
+        with the energy in MeV
+    energy_spectrum_sampling: String
+        "log" or None: the energy spectrum is sampled in log space
+        "linear" : the energy spectrum is sampled in linear space
+    ehpair_creation: float
+        the energy required to generate a electron-hole pair in eV
+        by default the Si value at room temperature is parsed i.e. 3.65 eV
+    material_density: float
+        the material density in g/cm3
+        by default he Si value at room temperature is parsed i.e. 2.3290 g/cm3
+    particle_direction: string
+        "isotropic" : particles are coming from all directions (outside of the sensor)
+        "orthogonal" : particles are coming from the top of the sensor (thickness = 0) and orthogonal to its surface
+    stopping_power_curve : String
+        the location of the file describing the total massive stopping power
+        energetic loss per mass of material and per unit path length versus particle energy
+        note that the the stopping power curve is assumed to be a csv file with two columns [energy, stopping power]
+        energy in MeV, stopping power in MeV cm2/g
+    """
 
     # determine the total number of ionizing particles to simulate based on flux and exposure duration
     n_p = np.int64(flux * exposure)
@@ -273,27 +277,44 @@ def simulate_charge_deposition(
         p_energies = np.random.normal(energy_mean, energy_spread, n_p)
     else:
         # from parsed spectrum using either logarithmic or linear sampling
-        energy_spectrum_data = np.loadtxt(energy_spectrum, dtype='float', comments='#')
+        energy_spectrum_data = np.loadtxt(energy_spectrum, dtype="float", comments="#")
         n_samples = 100000
         if energy_spectrum_sampling == "log":
-            log_energy_range = np.logspace(np.log10(np.min(energy_spectrum_data[:, 0])),
-                                           np.log10(np.max(energy_spectrum_data[:, 0])), n_samples)
-            log_energy_pdf = np.interp(log_energy_range, energy_spectrum_data[:, 0], energy_spectrum_data[:, 1])
+            log_energy_range = np.logspace(
+                np.log10(np.min(energy_spectrum_data[:, 0])),
+                np.log10(np.max(energy_spectrum_data[:, 0])),
+                n_samples,
+            )
+            log_energy_pdf = np.interp(
+                log_energy_range, energy_spectrum_data[:, 0], energy_spectrum_data[:, 1]
+            )
             log_energy_pdf /= np.sum(log_energy_pdf)
             p_energies = np.random.choice(log_energy_range, size=n_p, p=log_energy_pdf)
         else:
-            lin_energy_range = np.linspace(np.min(energy_spectrum_data[:, 0]), np.max(energy_spectrum_data[:, 0]),
-                                           n_samples)
-            energy_pdf = np.interp(lin_energy_range, energy_spectrum_data[:, 0], energy_spectrum_data[:, 1])
+            lin_energy_range = np.linspace(
+                np.min(energy_spectrum_data[:, 0]),
+                np.max(energy_spectrum_data[:, 0]),
+                n_samples,
+            )
+            energy_pdf = np.interp(
+                lin_energy_range, energy_spectrum_data[:, 0], energy_spectrum_data[:, 1]
+            )
             energy_pdf /= np.sum(energy_pdf)
             p_energies = np.random.choice(lin_energy_range, n_p, p=energy_pdf)
-    p_energies *= 1.e6  # from MeV to eV
+    p_energies *= 1.0e6  # from MeV to eV
 
     # extract stopping power data convert to correct units and compute initial deposited energy
-    stopping_power_data = np.genfromtxt(stopping_power_curve, skip_header=1, delimiter=',')
-    stopping_power_data[:, 0] *= 1.e6  # from MeV to eV
-    stopping_power_data[:, 1] *= material_density * 1.e2 * step_size  # from MeV cm2/g to eV
-    deposited_energies = np.interp(p_energies, stopping_power_data[:, 0], stopping_power_data[:, 1])
+    assert stopping_power_curve is not None
+    stopping_power_data = np.genfromtxt(
+        stopping_power_curve, skip_header=1, delimiter=","
+    )
+    stopping_power_data[:, 0] *= 1.0e6  # from MeV to eV
+    stopping_power_data[:, 1] *= (
+        material_density * 1.0e2 * step_size
+    )  # from MeV cm2/g to eV
+    deposited_energies = np.interp(
+        p_energies, stopping_power_data[:, 0], stopping_power_data[:, 1]
+    )
 
     tracks = []
     # for each particle generate and store the energy deposition track
@@ -305,11 +326,11 @@ def simulate_charge_deposition(
 
         # generate direction
         if particle_direction == "isotropic":
-            alpha = 2. * np.pi * np.random.random()
-            beta = 2. * np.pi * np.random.random()
+            alpha = 2.0 * np.pi * np.random.random()
+            beta = 2.0 * np.pi * np.random.random()
         else:
-            alpha = np.pi / 2.
-            beta = np.pi / 2.
+            alpha = np.pi / 2.0
+            beta = np.pi / 2.0
             # alpha = np.pi/2. + (np.random.randint(0,2))*np.pi
             # beta = np.pi/2. + (np.random.randint(0,2))*np.pi
 
@@ -320,7 +341,7 @@ def simulate_charge_deposition(
 
         # the particle is coming from outside the sensor: top or bottom, depending on travel direction (i.e. dz sign)
         if dz >= 0:
-            z = 0
+            z = 0.0  # type: float
         else:
             z = z_lim
 
@@ -341,11 +362,11 @@ def simulate_charge_deposition(
         # compute the minimum number of steps required to reach one of the sensor edge
         distances = np.array([distance_x, distance_y, distance_z])
         deltas = np.abs(np.array([dx, dy, dz]))
-        n_steps = np.int64(np.min(np.divide(distances, deltas)))
+        n_steps = int(np.min(np.divide(distances, deltas)))
 
         # check if incoming particle has enough energy to deposit all along the track or stops in the sensor
         if (e - n_steps * de) < 0:
-            n_steps = np.int64(e / de)
+            n_steps = int(e / de)
             # normally de should always be smaller than e
             # but it may happen that the particles would have stopped within a step_size
             # we assume that in that case the particle deposits its entire energy in one step
@@ -353,7 +374,9 @@ def simulate_charge_deposition(
                 n_steps = 1
             vde = np.zeros(n_steps)
             for step in range(n_steps):
-                vde[step] = np.interp(e, stopping_power_data[:, 0], stopping_power_data[:, 1])
+                vde[step] = np.interp(
+                    e, stopping_power_data[:, 0], stopping_power_data[:, 1]
+                )
                 e -= de
         else:  # otherwise assumes the deposited energy is not a significant fraction of input energy,
             # the input energy remains the same all along track as os the deposited energy
--- pyxel/util/materials.py
+++ pyxel/util/materials.py
@@ -10,7 +10,7 @@
 import numpy as np
 
 
-class Mercadtel():
+class Mercadtel:
     """Useful functions to compute properties of mercury cadmium telluride alloy."""
 
 
@@ -24,7 +24,7 @@ def lambda_e(lambda_cutoff: float) -> float:
 
     Returns
     -------
-    le: float
+    float
     """
     lambda_scale = 0.200847413  # um
     lambda_threshold = 4.635136423  # um
@@ -40,63 +40,201 @@ def lambda_e(lambda_cutoff: float) -> float:
     return le
 
 
-def energy_bandgap(cut_off):
+def energy_bandgap(cut_off: float):
+    """Compute the energy of the MCT bandgap as a function of its cut-off wavelength.
+
+    Parameters
+    ----------
+    cut_off: float
+        the cut_off wavelength in micrometer
+
+    Returns
+    -------
+    float
+        the bandgap energy in eV
     """
-    Computes the energy of the MCT bandgap as a function of its cut-off wavelength
-    :param cut_off: the cut_off wavelength in micrometer
-    :return: the bandgap energy in eV
-    """
+
     e_c = 1.24 / lambda_e(cut_off)
     return e_c
 
 
 def eg_hansen_inverse(eg, temperature):
-    """Hansen formulation inversion
-    Returns the cadmium fraction as a function of the temperature and the bandgap energy
+    """Return the cadmium fraction as a function of the temperature and the bandgap energy.
+
     Ref Biblio: G. L. HANSEN, J. L. SCHMIT, T. N. CASSELMAN, J. Appl. Phys., 53 (1982), p. 7099-7101
-    Eg: gap energy [eV]
-    temperature:  Temperature [K]
-    returns xx: MCT Cd concentration"""
+
+    Parameters
+    ----------
+    eg: float
+        gap energy [eV]
+    temperature: float
+        Temperature [K]
+
+    Returns
+    -------
+    float
+        MCT Cd concentration
+    """
     aa = 0.302
     bb = 1.93
-    cc = 0.535/1000
+    cc = 0.535 / 1000
     dd = 0.810
     ee = 0.832
-    xx = ((np.sqrt(-32. * cc ** 3. * ee * temperature ** 3 - (-27. * cc ** 2. * ee ** 2 + (36. * cc ** 2. * dd - 48. * bb * cc ** 2) * ee + 4. * cc ** 2. * dd ** 2) * temperature ** 2 - ((54. * cc * ee ** 2 - 36. * cc * dd * ee) * eg + 54. * aa * cc * ee ** 2 + ((-18. * bb - 36. * aa) * cc * dd + 24. * bb ** 2. * cc) * ee + 4. * cc * dd ** 3 - 4. * bb * cc * dd ** 2) * temperature + 27. * ee ** 2. * eg ** 2 - (-54. * aa * ee ** 2 + 18. * bb * dd * ee - 4. * dd ** 3) * eg + 27. * aa ** 2. * ee ** 2 - (18. * aa * bb * dd - 4. * bb ** 3) * ee + 4. * aa * dd ** 3 - bb ** 2. * dd ** 2)) / (6. * np.sqrt(3) * ee ** 2) + (cc * (18. * dd * ee * temperature - 27. * ee ** 2. * temperature) + 27. * ee ** 2. * eg + 27. * aa * ee ** 2 - 9. * bb * dd * ee + 2. * dd ** 3) / (54. * ee ** 3)) ** (1. / 3) - (-6. * cc * ee * temperature + 3. * bb * ee - dd ** 2) / (9. * ee ** 2. * ((np.sqrt(-32. * cc ** 3. * ee * temperature ** 3 - (-27. * cc ** 2. * ee ** 2 + (36. * cc ** 2. * dd - 48. * bb * cc ** 2) * ee + 4. * cc ** 2. * dd ** 2) * temperature ** 2 - ((54. * cc * ee ** 2 - 36. * cc * dd * ee) * eg + 54. * aa * cc * ee ** 2 + ((-18. * bb - 36. * aa) * cc * dd + 24. * bb ** 2. * cc) * ee + 4. * cc * dd ** 3 - 4. * bb * cc * dd ** 2) * temperature + 27. * ee ** 2. * eg ** 2 - (-54. * aa * ee ** 2 + 18. * bb * dd * ee - 4. * dd ** 3) * eg + 27. * aa ** 2. * ee ** 2 - (18. * aa * bb * dd - 4. * bb ** 3) * ee + 4. * aa * dd ** 3 - bb ** 2. * dd ** 2)) / (6. * np.sqrt(3) * ee ** 2) + (cc * (18. * dd * ee * temperature - 27. * ee ** 2. * temperature) + 27. * ee ** 2. * eg + 27. * aa * ee ** 2 - 9. * bb * dd * ee + 2. * dd ** 3) / (54. * ee ** 3)) ** (1. / 3)) + dd / (3. * ee)
+    xx = (
+        (
+            (
+                np.sqrt(
+                    -32.0 * cc**3.0 * ee * temperature**3
+                    - (
+                        -27.0 * cc**2.0 * ee**2
+                        + (36.0 * cc**2.0 * dd - 48.0 * bb * cc**2) * ee
+                        + 4.0 * cc**2.0 * dd**2
+                    )
+                    * temperature**2
+                    - (
+                        (54.0 * cc * ee**2 - 36.0 * cc * dd * ee) * eg
+                        + 54.0 * aa * cc * ee**2
+                        + ((-18.0 * bb - 36.0 * aa) * cc * dd + 24.0 * bb**2.0 * cc)
+                        * ee
+                        + 4.0 * cc * dd**3
+                        - 4.0 * bb * cc * dd**2
+                    )
+                    * temperature
+                    + 27.0 * ee**2.0 * eg**2
+                    - (-54.0 * aa * ee**2 + 18.0 * bb * dd * ee - 4.0 * dd**3) * eg
+                    + 27.0 * aa**2.0 * ee**2
+                    - (18.0 * aa * bb * dd - 4.0 * bb**3) * ee
+                    + 4.0 * aa * dd**3
+                    - bb**2.0 * dd**2
+                )
+            )
+            / (6.0 * np.sqrt(3) * ee**2)
+            + (
+                cc * (18.0 * dd * ee * temperature - 27.0 * ee**2.0 * temperature)
+                + 27.0 * ee**2.0 * eg
+                + 27.0 * aa * ee**2
+                - 9.0 * bb * dd * ee
+                + 2.0 * dd**3
+            )
+            / (54.0 * ee**3)
+        )
+        ** (1.0 / 3)
+        - (-6.0 * cc * ee * temperature + 3.0 * bb * ee - dd**2)
+        / (
+            9.0
+            * ee**2.0
+            * (
+                (
+                    np.sqrt(
+                        -32.0 * cc**3.0 * ee * temperature**3
+                        - (
+                            -27.0 * cc**2.0 * ee**2
+                            + (36.0 * cc**2.0 * dd - 48.0 * bb * cc**2) * ee
+                            + 4.0 * cc**2.0 * dd**2
+                        )
+                        * temperature**2
+                        - (
+                            (54.0 * cc * ee**2 - 36.0 * cc * dd * ee) * eg
+                            + 54.0 * aa * cc * ee**2
+                            + (
+                                (-18.0 * bb - 36.0 * aa) * cc * dd
+                                + 24.0 * bb**2.0 * cc
+                            )
+                            * ee
+                            + 4.0 * cc * dd**3
+                            - 4.0 * bb * cc * dd**2
+                        )
+                        * temperature
+                        + 27.0 * ee**2.0 * eg**2
+                        - (-54.0 * aa * ee**2 + 18.0 * bb * dd * ee - 4.0 * dd**3)
+                        * eg
+                        + 27.0 * aa**2.0 * ee**2
+                        - (18.0 * aa * bb * dd - 4.0 * bb**3) * ee
+                        + 4.0 * aa * dd**3
+                        - bb**2.0 * dd**2
+                    )
+                )
+                / (6.0 * np.sqrt(3) * ee**2)
+                + (
+                    cc * (18.0 * dd * ee * temperature - 27.0 * ee**2.0 * temperature)
+                    + 27.0 * ee**2.0 * eg
+                    + 27.0 * aa * ee**2
+                    - 9.0 * bb * dd * ee
+                    + 2.0 * dd**3
+                )
+                / (54.0 * ee**3)
+            )
+            ** (1.0 / 3)
+        )
+        + dd / (3.0 * ee)
+    )
     return xx
 
 
 def eg_hansen(x, temperature):
-    """
-    Computes the MCT Energy gap computation as a function of the Cd fraction
+    """Compute the MCT Energy gap computation as a function of the Cd fraction.
+
     Ref Biblio: G. L. HANSEN, J. L. SCHMIT, T. N. CASSELMAN, J. Appl. Phys., 53 (1982), p. 7099-7101
-    x: MCT Cd concentration
-    temperature: temperature [K]
-    Returns eg: gap energy [eV]
+
+    Parameters
+    ----------
+    x: float
+    MCT Cd concentration (should be smaller than 1)
+    temperature: float
+        temperature [K]
+
+    Returns
+    -------
+    float
+         gap energy [eV]
     """
-    eg = -0.302+1.93*x-0.810*x**2+0.832*x**3+0.535*(1-2*x)*temperature/1000
+    eg = (
+        -0.302
+        + 1.93 * x
+        - 0.810 * x**2
+        + 0.832 * x**3
+        + 0.535 * (1 - 2 * x) * temperature / 1000
+    )
     return eg
 
 
 def absorption_coefficient(e, x, temperature):
-    """
-    computation of the absorption coefficient of MCT alloy using a non parabolic band model and an urbach tail
+    """Compute the absorption coefficient of MCT alloy using a non-parabolic band model and an urbach tail.
+
     Ref: Moazzami, JEM 34(6) p773 (2005)
-    :param e: photon energy [eV]
-    :param x: MCT Cd composition fraction
-    :param temperature: temperature [K]
-    :return: the absorbtion coefficient
+
+    Parameters
+    ----------
+    e: float
+        photon energy [eV]
+    x: float
+        MCT Cd composition fraction (should be smaller than one)
+    temperature: float
+        temperature [K]
+
+    Returns
+    -------
+    float
+        the absorption coefficient
     """
+
     w = 4.96e-3
-    K = -20060 + 115750 * x + 32.43 * temperature - 64170 * x ** 2 + 0.43231 * temperature ** 2 - 101.92 * x * temperature
+    k = (
+        -20060
+        + 115750 * x
+        + 32.43 * temperature
+        - 64170 * x**2
+        + 0.43231 * temperature**2
+        - 101.92 * x * temperature
+    )
     n = 0.74487 - 0.44513 * x + 1e-4 * (7.99 - 7.57 * x) * temperature
-    Eg = eg_hansen(x, temperature)
-    delta = (Eg + w * (n - 1)) ** 2 + 4 * w * Eg
-    Eo = (Eg + w * (n - 1) + np.sqrt(delta)) / 2
-    alphao = K * (Eo - Eg) ** n / Eo
-    alpha1 = alphao * np.exp((e - Eo) / w)
-    alpha2 = K * (e - Eg) ** n / e
-    if e < Eo:
+    eg = eg_hansen(x, temperature)
+    delta = (eg + w * (n - 1)) ** 2 + 4 * w * eg
+    eo = (eg + w * (n - 1) + np.sqrt(delta)) / 2
+    alphao = k * (eo - eg) ** n / eo
+    alpha1 = alphao * np.exp((e - eo) / w)
+    alpha2 = k * (e - eg) ** n / e
+    if e < eo:
         alpha = alpha1
     else:
         alpha = alpha2
@@ -104,14 +242,21 @@ def absorption_coefficient(e, x, temperature):
 
 
 def density(x):
-    """
-    Compute the MCT density as a function of the cadmium fraction
-    :param x: the cadmium fraction
-    :return: the density in kg/m3
+    """Compute the MCT density as a function of the cadmium fraction.
+
+    Parameters
+    ----------
+    x: float
+        the cadmium fraction
+
+    Returns
+    -------
+    float
+    the density in g/cm3
     """
     me_density = 5.43
     cd_density = 8.65
     te_density = 6.24
-    mct_density = 0.5*(x/cd_density + (1-x)/me_density) + 0.5/te_density
-    mct_density = 1./mct_density
+    mct_density = 0.5 * (x / cd_density + (1 - x) / me_density) + 0.5 / te_density
+    mct_density = 1.0 / mct_density
     return mct_density
