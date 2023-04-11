bb0f725c17c57278a208c23460e3f8b52b4a34b4
constanze.seibert <constanze.seibert@esa.int>
2023-04-11 15:16:21 +0200

    Add units in dropdown menu.

--- pyxel/notebook/jupyxel.py
+++ pyxel/notebook/jupyxel.py
@@ -161,17 +161,17 @@ def display_detector(detector: "Detector") -> "Layout":
     # Container for detector data, leave out where there is none.
     det: dict[str, np.ndarray] = {}
     if detector.input_image is not None:
-        det["Input"] = detector.input_image
+        det["Input Image"] = detector.input_image
     if detector._photon is not None:
-        det["Photon"] = detector.photon.array
+        det["Photon [ph]"] = detector.photon.array
     if detector._charge is not None:
-        det["Charge"] = detector.charge.array
+        det["Charge [e-]"] = detector.charge.array
     if detector._pixel is not None:
-        det["Pixel"] = detector.pixel.array
+        det["Pixel [e-]"] = detector.pixel.array
     if detector._signal is not None:
-        det["Signal"] = detector.signal.array
+        det["Signal [V]"] = detector.signal.array
     if detector._image is not None:
-        det["Image"] = detector.image.array
+        det["Image [ADU]"] = detector.image.array
 
     def get_image(name):
         data: np.ndarray = det[name]
