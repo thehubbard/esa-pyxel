c352d44c5c13550886451fbc6e42d883ee9fe273
Frederic Lemmel <frederic.lemmel@ext.esa.int>
2024-07-19 12:58:03 +0200

    Add new unit tests for parallel CDM.

--- tests/models/charge_transfer/test_cdm.py
+++ tests/models/charge_transfer/test_cdm.py
@@ -11,6 +11,7 @@ from typing import Literal, Optional
 
 import numpy as np
 import pytest
+from astropy import constants
 
 from pyxel.detectors import (
     CCD,
@@ -21,6 +22,7 @@ from pyxel.detectors import (
     Environment,
 )
 from pyxel.models.charge_transfer import cdm
+from pyxel.models.charge_transfer.cdm import run_cdm_parallel
 
 
 @pytest.fixture
@@ -35,7 +37,7 @@ def ccd_5x5() -> CCD:
             pixel_horz_size=10.0,
         ),
         environment=Environment(temperature=273.15),
-        characteristics=Characteristics(),
+        characteristics=Characteristics(full_well_capacity=100_000),
     )
 
 
@@ -277,6 +279,125 @@ def test_cdm_parallel(
     )
 
     np.testing.assert_array_almost_equal(detector.pixel.array, expected)
+
+
+def test_model_cdm_parallel(ccd_5x5: CCD):
+    pixel_2d = np.array(
+        [
+            [47151.0, 50709.0, 43162.0, 52592.0, 49057.0],
+            [46403.0, 50111.0, 49019.0, 57831.0, 45687.0],
+            [44410.0, 51329.0, 51965.0, 45841.0, 46208.0],
+            [49097.0, 51927.0, 49963.0, 57601.0, 52459.0],
+            [49765.0, 48042.0, 52507.0, 44827.0, 38582.0],
+        ],
+        dtype=float,
+    )
+
+    detector = ccd_5x5
+    detector.pixel.array = pixel_2d
+
+    cdm(
+        detector=detector,
+        direction="parallel",
+        trap_release_times=[3.0e-2, 0.5e-1],
+        trap_densities=[20.0, 100.0],
+        sigma=[1.0e-10, 1e-10],
+        beta=0.3,
+        max_electron_volume=1.62e-10,
+        transfer_period=9.4722e-04,
+        charge_injection=False,
+    )
+
+    expected = np.array(
+        [
+            [47151.0, 50709.0, 43162.0, 52592.0, 49057.0],
+            [46216.9295795, 49920.5571595, 48829.8205124, 57632.1361343, 45501.8019372],
+            [44228.8403519, 51135.8123811, 51769.1304319, 45669.0288204, 46021.5555777],
+            [48896.5803797, 51733.1976991, 49777.2530026, 57376.2270095, 52251.5396709],
+            [49572.6225729, 47867.2693776, 52305.4188107, 44685.6475847, 38456.6457955],
+        ]
+    )
+
+    new_pixel = detector.pixel.array
+    np.testing.assert_array_almost_equal(new_pixel, expected)
+
+
+@pytest.mark.parametrize(
+    "with_numba",
+    [
+        pytest.param(True, id="With Numba"),
+        pytest.param(False, id="Without Numba"),
+    ],
+)
+def test_run_cdm_parallel(with_numba: bool):
+    pixel_2d = np.array(
+        [
+            [47151.0, 50709.0, 43162.0, 52592.0, 49057.0],
+            [46403.0, 50111.0, 49019.0, 57831.0, 45687.0],
+            [44410.0, 51329.0, 51965.0, 45841.0, 46208.0],
+            [49097.0, 51927.0, 49963.0, 57601.0, 52459.0],
+            [49765.0, 48042.0, 52507.0, 44827.0, 38582.0],
+        ],
+    )
+
+    electron_effective_mass = 0.5
+    full_well_capacity = 100_000  # unit: electron
+    temperature = 273.15  # unit: K
+    max_electron_volume = 1.62e-10  # unit: cm3
+    transfer_period = 0.00094722  # unit: s
+    beta = 0.3
+    trap_release_times = np.array([0.03, 0.05])  # unit: s
+    trap_densities = np.array([20.0, 100.0])  # unit: electron
+    sigma = np.array([1e-10, 1e-10])  # unit: cm2 / electron
+
+    e_effective_mass = electron_effective_mass * constants.m_e.value
+    e_thermal_velocity = np.sqrt(
+        3 * constants.k_B.value * temperature / e_effective_mass
+    )
+
+    if with_numba:
+        # Test with 'numba'
+        new_pixel_2d = run_cdm_parallel(
+            array=pixel_2d,
+            vg=max_electron_volume,
+            t=transfer_period,
+            fwc=full_well_capacity,
+            vth=e_thermal_velocity,
+            charge_injection=False,
+            chg_inj_parallel_transfers=5,
+            beta=beta,
+            tr=trap_release_times,
+            nt=trap_densities,
+            sigma=sigma,
+        )
+    else:
+        # Test without 'numba'
+        new_pixel_2d = run_cdm_parallel.py_func(
+            array=pixel_2d,
+            vg=max_electron_volume,
+            t=transfer_period,
+            fwc=full_well_capacity,
+            vth=e_thermal_velocity,
+            charge_injection=False,
+            chg_inj_parallel_transfers=5,
+            beta=beta,
+            tr=trap_release_times,
+            nt=trap_densities,
+            sigma=sigma,
+        )
+
+    expected = np.array(
+        [
+            [47151.0, 50709.0, 43162.0, 52592.0, 49057.0],
+            [46216.9295795, 49920.5571595, 48829.8205124, 57632.1361343, 45501.8019372],
+            [44228.8403519, 51135.8123811, 51769.1304319, 45669.0288204, 46021.5555777],
+            [48896.5803797, 51733.1976991, 49777.2530026, 57376.2270095, 52251.5396709],
+            [49572.6225729, 47867.2693776, 52305.4188107, 44685.6475847, 38456.6457955],
+        ],
+    )
+
+    assert isinstance(new_pixel_2d, np.ndarray)
+    np.testing.assert_array_almost_equal(np.asarray(new_pixel_2d), np.asarray(expected))
 
 
 @pytest.mark.parametrize(
